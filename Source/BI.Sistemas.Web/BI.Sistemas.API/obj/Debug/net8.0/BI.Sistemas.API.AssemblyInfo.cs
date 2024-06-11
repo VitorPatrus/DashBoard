09210fe73c2cf72bba7248e208252742e2296d21
Vitor Fernandes de Souza <111388245+VitorFernandesSouza@users.noreply.github.com>
2024-06-11 10:15:06 -0300

    meu primeiro commit

--- Source/BI.Sistemas.Web/BI.Sistemas.API/obj/Debug/net8.0/BI.Sistemas.API.AssemblyInfo.cs
+++ Source/BI.Sistemas.Web/BI.Sistemas.API/obj/Debug/net8.0/BI.Sistemas.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BI.Sistemas.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c7117020378613660f63925abc6a2fbbf36a54")]
 [assembly: System.Reflection.AssemblyProductAttribute("BI.Sistemas.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BI.Sistemas.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
