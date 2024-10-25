a449562dd9291e432bc2f2063a1494b77cab47ce
Vitor Fernandes de Souza <111388245+VitorFernandesSouza@users.noreply.github.com>
2024-10-25 10:55:28 -0300

    update
    update

--- Source/BI.Sistemas.Web/BI.Sistemas.API/obj/Debug/net8.0/BI.Sistemas.API.AssemblyInfo.cs
+++ Source/BI.Sistemas.Web/BI.Sistemas.API/obj/Debug/net8.0/BI.Sistemas.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BI.Sistemas.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ee8e9dd9fadd56ad9f107333960bb7cf979bd21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30c3db316960b325c99d086fa5340da4eea16fa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BI.Sistemas.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BI.Sistemas.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
